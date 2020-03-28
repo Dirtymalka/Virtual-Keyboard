03ed9250d8f23583af7e04cb1f5b5d1baeb5709a
Artem <artem.antonov1993@gmail.com>
2020-03-28 23:42:02 +0300

    feat: add click-events and add an ability to change language

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ const textarea = document.createElement('textarea');
 textarea.classList.add('textarea');
 textarea.setAttribute('autofocus', '');
 document.body.append(textarea);
-const textareaContent = document.querySelector('.textarea');
+
 // Create Container
 const container = document.createElement('div');
 container.className = 'container';
 document.body.append(container);
-
 const containerBoard = document.querySelector('.container');
 
 const keyLayoutEnglishUnShift = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='];
@@ -21,7 +20,7 @@ const keyLayoutEnglishKeys = [
 ];
 
 const keyLayoutRussianUnShift = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='];
-const keyLayoutRussianShift = ['', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+'];
+const keyLayoutRussianShift = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+'];
 const keyLayoutRussianKeys = [
   'backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', '[', ']', '\\', '|',
   'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
@@ -31,7 +30,9 @@ const keyLayoutRussianKeys = [
 
 
 // Create English Keys
-function createEnglishKeys(keyLayoutUnShift, keyLayoutShift, keyLayoutKeys) {
+function createKeys(keyLayoutUnShift, keyLayoutShift, keyLayoutKeys) {
+  if (keyLayoutUnShift === keyLayoutEnglishUnShift) containerBoard.classList.add('english');
+  if (keyLayoutUnShift === keyLayoutRussianUnShift) containerBoard.classList.add('russian');
   for (let i = 0; i < keyLayoutUnShift.length; i += 1) {
     const div = document.createElement('div');
     div.className = 'key tap-shift';
@@ -104,12 +105,35 @@ function createEnglishKeys(keyLayoutUnShift, keyLayoutShift, keyLayoutKeys) {
   });
 }
 
-createEnglishKeys(keyLayoutEnglishUnShift, keyLayoutEnglishShift, keyLayoutEnglishKeys);
+createKeys(keyLayoutRussianUnShift, keyLayoutRussianShift, keyLayoutRussianKeys);
+
+// Language Change
+function languageChange() {
+  if (containerBoard.classList.contains('russian')) {
+    containerBoard.classList.remove('russian');
+    containerBoard.innerHTML = '';
+    createKeys(keyLayoutEnglishUnShift, keyLayoutEnglishShift, keyLayoutEnglishKeys);
+  } else if (containerBoard.classList.contains('english')) {
+    containerBoard.classList.remove('english');
+    containerBoard.innerHTML = '';
+    createKeys(keyLayoutRussianUnShift, keyLayoutRussianShift, keyLayoutRussianKeys);
+  }
+}
 
 
 // Virtual field input
 containerBoard.addEventListener('mousedown', (event) => {
-  let activeButton = event.target.closest('div');
+  document.querySelector('.textarea').focus();
+  const activeButton = event.target.closest('div');
+  if (activeButton.classList.contains('space')) {
+    textarea.value += ' ';
+  }
+  if (activeButton.classList.contains('tab')) {
+    textarea.value += '  ';
+  }
+  if (activeButton.classList.contains('enter')) {
+    textarea.value += '\n';
+  }
   if (activeButton.classList.contains('special')) return;
   if (!activeButton.classList.contains('key')) return;
   if (!activeButton) return;
@@ -119,36 +143,96 @@ containerBoard.addEventListener('mousedown', (event) => {
 });
 
 
-//
-const keyShift = containerBoard.querySelector('.shift');
-keyShift.addEventListener('mousedown', () => {
-  keyShift.classList.toggle('active');
-  const keys = containerBoard.querySelectorAll('div');
-  if (keyShift.classList.contains('active')) {
-    keys.forEach((letter) => {
-      if (!letter.classList.contains('tap-shift') && !letter.classList.contains('special')) {
-        letter.textContent = letter.textContent.toUpperCase();
-      }
-    });
-  } else {
-    keys.forEach((letter) => {
-      if (!letter.classList.contains('tap-shift') && !letter.classList.contains('special')) {
-        letter.textContent = letter.textContent.toLowerCase();
-      }
-    });
+// Click on Special Keys
+
+containerBoard.addEventListener('mousedown', (event) => {
+  // Click on CapsLock
+  if (event.target.classList.contains('capslock')) {
+    const keyCapsLock = containerBoard.querySelector('.capslock');
+    keyCapsLock.classList.toggle('active');
+    const keys = containerBoard.querySelectorAll('div');
+    if (keyCapsLock.classList.contains('active')) {
+      keys.forEach((letter) => {
+        const letterF = letter;
+        if (!letter.classList.contains('tap-shift') && !letter.classList.contains('special')) {
+          letterF.textContent = letter.textContent.toUpperCase();
+        }
+      });
+    } else {
+      keys.forEach((letter) => {
+        const letterF = letter;
+        if (!letter.classList.contains('tap-shift') && !letter.classList.contains('special')) {
+          letterF.textContent = letter.textContent.toLowerCase();
+        }
+      });
+    }
   }
 
-  const tapShiftKeys = containerBoard.querySelectorAll('.tap-shift');
-  tapShiftKeys.forEach((key) => {
+  const keysAlt = containerBoard.querySelectorAll('.alt');
+  // Click on Shift
+  const keysShift = containerBoard.querySelectorAll('.shift');
+  if (event.target.classList.contains('shift')) {
+    event.target.classList.toggle('active');
+    const keys = containerBoard.querySelectorAll('div');
+    if (event.target.classList.contains('active')) {
+      keys.forEach((letter) => {
+        const letterF = letter;
+        if (!letter.classList.contains('tap-shift') && !letter.classList.contains('special')) {
+          letterF.textContent = letter.textContent.toUpperCase();
+        }
+      });
+    } else {
+      keys.forEach((letter) => {
+        const letterF = letter;
+        if (!letter.classList.contains('tap-shift') && !letter.classList.contains('special')) {
+          letterF.textContent = letter.textContent.toLowerCase();
+        }
+      });
+    }
 
-  });
+    const tapShiftKeys = containerBoard.querySelectorAll('.tap-shift');
+    tapShiftKeys.forEach((key) => {
+      const keyS = key;
+      const supKeyContent = key.querySelector('sup').textContent;
+      const spanKeyContent = key.querySelector('span').textContent;
+      keyS.querySelector('sup').textContent = spanKeyContent;
+      keyS.querySelector('span').textContent = supKeyContent;
+    });
+
+    if ((keysAlt[0].classList.contains('active') || keysAlt[1].classList.contains('active')) && (keysShift[0].classList.contains('active') || keysShift[1].classList.contains('active'))) {
+      languageChange();
+    }
+  }
+
+  // Click on Alt
+  if (event.target.classList.contains('alt')) {
+    event.target.classList.toggle('active');
+    if ((keysAlt[0].classList.contains('active') || keysAlt[1].classList.contains('active')) && (keysShift[0].classList.contains('active') || keysShift[1].classList.contains('active'))) {
+      languageChange();
+    }
+  }
+  if (event.target.classList.contains('backspace')) {
+    textarea.value = textarea.value.slice(0, textarea.value.length - 1);
+  }
+
+  // Click on Ctrl
+  if (event.target.classList.contains('ctrl')) {
+    event.target.classList.toggle('active');
+  }
+
+  // Click on Win
+  if (event.target.classList.contains('win')) {
+    event.target.classList.toggle('active');
+    containerBoard.querySelectorAll('.key').forEach((elem) => {
+      elem.classList.toggle('other-keys');
+    });
+  }
 });
 
-
-
-
-
-
+// const backspace = containerBoard.querySelector('.backspace');
+// backspace.addEventListener('click', () => {
+//   textarea.value = textarea.value
+// });
 
 // document.addEventListener('keydown', (event) => {
 //   let a = containerBoard.querySelectorAll('div');
